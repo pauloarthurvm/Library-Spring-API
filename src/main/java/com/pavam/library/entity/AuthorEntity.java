30c38b69ea2b0ecac4039fa8e84564bdb8b05fb4
Paulo Varassin <pauloarthurvm@hotmail.com>
2026-01-27 20:36:09 -0300

    Create additional entities - Books, Authors, Reviews

---
+++ src/main/java/com/pavam/library/entity/AuthorEntity.java
@@ -0,0 +1,35 @@
+package com.pavam.library.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "AUTHORS")
+public class AuthorEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/pavam/library/entity/ReviewEntity.java
@@ -0,0 +1,35 @@
+package com.pavam.library.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "REVIEWS")
+public class ReviewEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String review;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+}
